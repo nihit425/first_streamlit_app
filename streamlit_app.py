804a9e13730e1cea9e53a52971b6b4be943de8f2
Nihit <37477665+nihit425@users.noreply.github.com>
2023-11-21 20:29:29 +0530

    added 3 new items to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blue Berry Oatmeal')
-streamlit.text('Spinach Sandwith with Soup')
-streamlit.text('Halfried with Boiled Eggs')
-
+streamlit.text('🥣 Omega 3 and Blue Berry Oatmeal')
+streamlit.text('🥗 Spinach Sandwith with Soup')
+streamlit.text('🐔 Halfried with Boiled Eggs')
+streamlit.text('🥑 Avocado Toast')
+streamlit.text('🍞 Bombay Cheese Sandwich')
