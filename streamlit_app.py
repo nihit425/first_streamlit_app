e23faac5c9ae0862062ad981795fbf913b47aa71
Nihit <37477665+nihit425@users.noreply.github.com>
2023-11-21 20:25:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blue Berry Oatmeal')
+streamlit.text('Spinach Sandwith with Soup')
+streamlit.text('Halfried with Boiled Eggs')
