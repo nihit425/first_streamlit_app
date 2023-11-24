d30627be47170232dfc6e1959e1b5748f2b239b7
Nihit <37477665+nihit425@users.noreply.github.com>
2023-11-24 22:08:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" +fruit_choice)
+
 streamlit.dataframe(fruityvice_normalized)
