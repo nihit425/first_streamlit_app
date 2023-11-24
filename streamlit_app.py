2c1fc9d312229fdbb8ef5ad7f0cd080f04880c1d
Nihit <37477665+nihit425@users.noreply.github.com>
2023-11-25 00:39:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_row = get_fruit_load_list()
   streamlit.dataframe(my_data_row)
 
+streamlit.stop()
 second_fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', second_fruit_choice)
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values second_fruit_choice" )
